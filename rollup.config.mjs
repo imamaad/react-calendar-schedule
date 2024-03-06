760de6abdb6ff4e7e3acb3c9792ca804565e43b3
mohammad hossin rezazadeh <imamaad.ir@gmail.com>
2024-03-06 20:03:35 +0330

    install react-virtualized

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -29,7 +29,7 @@ export default [
             typescript({tsconfig: "./tsconfig.json"}),
             postcss()
         ],
-        external: ["react", "react-dom", "moment", "lodash"],
+        external: ["react", "react-dom", "moment", "lodash", "react-virtualized"],
     },
     {
         input: "dist/esm/types/index.d.ts",
