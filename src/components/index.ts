1eca0a8e061367f7f5b488db886fa27cbb59cdb5
mohammad hossin rezazadeh <imamaad.ir@gmail.com>
2024-03-11 15:35:05 +0330

    update version

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,3 @@
 export {default as CalendarSchedule} from "./CalendarSchedule";
-export {RangeCalendarSchedule} from "./RangeCalendarSchedule";
 export {RangeCalendarScheduleVirtualized} from "./RangeCalendarScheduleVirtualized";
 
